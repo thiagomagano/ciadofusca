005a6f5c79bf549e8559432170547f4b8f87d448
Thiago Magano <thiagomagano1993@outlook.com>
2023-06-30 14:00:24 -0300

    feature: validation interesses form

--- src/routes/(app)/carros/[id]/+page.server.js
+++ src/routes/(app)/carros/[id]/+page.server.js
@@ -3,6 +3,10 @@ import { PUBLIC_POCKETBASE_URL } from '$env/static/public'
 import { PB_ADMIN_USER, PB_ADMIN_PASS } from '$env/static/private';
 
 
+
+
+
+
 export async function load({ params, fetch }) {
   const response = await fetch(PUBLIC_POCKETBASE_URL + `/api/collections/carros/records/` + params.id)
   const jsonData = await response.json()
@@ -20,9 +24,12 @@ export const actions = {
     const whatsapp = data.get('whatsapp');
     const email = data.get('email');
 
+    const whatsappRegex = new RegExp(/^(?:\+|00)?(?:55)?(?:\(?0?\d{2}\)?)?[9]?\d{4}[-.]?\d{4}$/);
 
-    if (!nome || !whatsapp) { return fail(400, { missing: true, msg: 'Faltou preencher campos obrigatórios' }); }
-    if (whatsapp.match(/[^0-9]/g)) { return fail(400, { incorrect: true, msg: 'Whatsapp deve conter apenas números' }); }
+
+    if (!nome) { return fail(400, { missingName: true, msg: 'O campo Nome é obrigatórios' }); }
+    if (!whatsapp) { return fail(400, { missingWpp: true, msg: 'O campo Whatsapp obrigatórios' }); }
+    if (!whatsapp.match(whatsappRegex)) { return fail(400, { incorrect: true, msg: "O numéro deve estar em um formato válido ex: 51987654321" }); }
 
     const pbData = {
       "nome": nome,
@@ -31,11 +38,15 @@ export const actions = {
       "idCarro": params.id
     };
 
-    const record = locals.pb.admins.authWithPassword(PB_ADMIN_USER, PB_ADMIN_PASS).then(async (res) => {
-      return await locals.pb.collection('interesses').create(pbData)
-    }).catch((err) => { { return fail(400, 'Erro ao criar registro'); } })
+
+    await locals.pb.admins.authWithPassword(PB_ADMIN_USER, PB_ADMIN_PASS)
+
+    const record = await locals.pb.collection('interesses').create(pbData)
+
+    if (!record) throw error(400, { msg: 'Erro ao criar registro' })
 
     return { success: true, record: JSON.stringify(record) };
+
   }
 }
 
--- src/routes/(app)/carros/[id]/DialogForm.svelte
+++ src/routes/(app)/carros/[id]/DialogForm.svelte
@@ -1,28 +1,46 @@
 <script>
 	import { Button, Modal, Label, Input, Checkbox } from 'flowbite-svelte';
+
+	import { enhance } from '$app/forms';
 	export let isOpen;
 	export let carro;
+
+	export let form;
 </script>
 
 <Modal bind:open={isOpen} size="xs" autoclose={false} class="w-full" outsideclose={true}>
-	<form class="flex flex-col space-y-6" method="POST">
-		<h3 class="text-xl font-medium text-gray-900 dark:text-white">
-			Registre seu interesse no {carro.titulo}
-		</h3>
-		<p>Para isso, precisamos de algumas informações:</p>
-		<Label class="space-y-2">
-			<span>Seu Nome *</span>
-			<Input type="text" name="nome" placeholder="Fulano da silva" required />
-		</Label>
-		<Label class="space-y-2">
-			<span>Seu Whatsapp *</span>
-			<Input type="text" name="whatsapp" placeholder="5199999999" required />
-		</Label>
-		<Label class="space-y-2">
-			<span>Seu Email</span>
-			<Input type="email" name="email" placeholder="fulano@gmail.com" />
-		</Label>
+	{#if !form?.success}
+		<form class="flex flex-col space-y-6" method="POST" use:enhance>
+			<h3 class="text-xl font-medium text-gray-900 dark:text-white">
+				Registre seu interesse no {carro.titulo}
+			</h3>
+			<p>Para isso, precisamos de algumas informações:</p>
+			<Label class="space-y-2">
+				<span>Seu Nome *</span>
+				<Input type="text" name="nome" placeholder="Fulano da silva" />
+				{#if form?.missingName}
+					<p class="text-red-500">{form?.msg}</p>
+				{/if}
+			</Label>
+			<Label class="space-y-2">
+				<span>Seu Whatsapp *</span>
+				<Input type="text" name="whatsapp" placeholder="5199999999" />
+				{#if form?.missingWpp}
+					<p class="text-red-500">{form?.msg}</p>
+				{/if}
+				{#if form?.incorrect}
+					<p class="text-red-500">{form?.msg}</p>
+				{/if}
+			</Label>
+			<Label class="space-y-2">
+				<span>Seu Email</span>
+				<Input type="email" name="email" placeholder="fulano@gmail.com" />
+			</Label>
 
-		<Button type="submit" class="w-full1 bg-primary-50 hover:bg-primary-300">Registrar</Button>
-	</form>
+			<Button type="submit" class="w-full1 bg-primary-50 hover:bg-primary-300">Registrar</Button>
+		</form>
+	{:else}
+		<h3>Formulário enviado com sucesso!</h3>
+		<p>Em breve entraremos em contato com você.</p>
+	{/if}
 </Modal>
